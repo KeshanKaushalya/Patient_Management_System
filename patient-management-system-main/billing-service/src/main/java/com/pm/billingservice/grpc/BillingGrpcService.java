0e220549520bbb1b9f7e95be260a79adef0d1d8e
KeshanKaushalya <madusankakeshan782@gmail.com>
2025-08-03 16:17:53 +0530

    Update BillingGrpcService.java

--- patient-management-system-main/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ patient-management-system-main/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,6 +1,6 @@
 package com.pm.billingservice.grpc;
 
-import billing.BillingResponse;
+import cbilling.BillingResponse;
 import billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
